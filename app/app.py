b28e6f86135ad08dbda85a22d520b0842c615c24
MerelyOneMax1986 <71835176+MerelyOneMax1986@users.noreply.github.com>
2021-05-27 23:49:49 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -17,7 +17,6 @@ try:
     # Set DEBUG flag using Consul KV store
     index, data = consul_client.kv.get('web/debug')
     DEBUG = data.get('Value', True)
-    break
 except ValueError:
     print("Oops!  That was no valid number.  Try again...")
 
